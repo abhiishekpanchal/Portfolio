414957f62b28e3a113892b6916fdda28957e6ac9
abhiishekpanchal <abhishekp.280403@gmail.com>
2024-04-22 01:10:39 +0530

    portfolio

---
+++ src/constants/constants.js
@@ -0,0 +1,309 @@
+import {
+  mobile,
+  backend,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  nextjs,
+  express,
+  java,
+  jwt
+} from "../assets";
+import abc from "../assets/Quickmeet.png"
+import def from "../assets/LMS.png"
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Projects",
+  },
+  {
+    id: "experience",
+    title: "Experience",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+ 
+];
+
+const services = [
+  {
+    title: "Frontend Developer",
+    icon: web,
+  },
+  {
+    title: "Backend Developer",
+    icon: mobile,
+  },
+  {
+    title: "Programmer",
+    icon: backend,
+  },
+  // {
+  //   title: "Creator",
+  //   icon: creator,
+  // },
+];
+
+const technologies = [
+  {
+    name: "React JS",
+    icon: reactjs,
+    position: [0, 0, 0],
+  },
+  {
+    name: "Next JS",
+    icon: nextjs,
+    position: [2, 0, 0],
+  },
+  {
+    name: "Express JS",
+    icon: express,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  // {
+  //   name: "Three JS",
+  //   icon: threejs,
+  // },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "Java",
+    icon: java,
+  },
+  {
+    name: "JWT",
+    icon: jwt,
+  },
+  // {
+  //   name: "NextAuth",
+  //   icon: nextauth,
+  // },
+  // {
+  //   name: "figma",
+  //   icon: figma,
+  // },
+  // {
+  //   name: "docker",
+  //   icon: docker,
+  // },
+];
+
+const projects = [
+  {
+    name: "Quickmeet",
+    description:
+      "A web app integrating video calls and collaborative whiteboarding for real-time virtual collaboration, built with HTML, CSS, JavaScript, Socket.io, WebRTC.",
+    features: [
+      "Users can create a room and invite others to join the room using a unique room code.",
+      "Users can draw on the whiteboard and the changes will be reflected in real-time to all the users in the room.",
+    ],
+    tags: [
+      {
+        name: "HTML",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "CSS",
+        color: "green-text-gradient",
+      },
+      {
+        name: "JavaScript",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "Socket.io",
+        color: "orange-text-gradient",
+      },
+      {
+        name: "WebRTC",
+        color: "blue-text-gradient",
+      },
+    ],
+    image: abc,
+    source_code_link: "https://github.com/abhiishekpanchal/Meet-WhiteBoard",
+    live_link: "https://meet-whiteboard.onrender.com/",
+  },
+  {
+    name: "Library Management System",
+    description:
+      "A comprehensive web-based Library Management System that is built using Springboot framework and MySQL database.",
+    features: [
+      "User authentication using NextAuth.js with JWT and OAuth providers.",
+      "Redux to Manage app state effectively.",
+      "Play, pause, skip, and control the playback of songs.",
+      "Users can Create, update and delete their own playlists.",
+      "Add songs to favorites in one click.",
+      "Auto adds similar songs to the queue.",
+      "Swipe and touch gestures for touch-responsive devices.",
+      "Display song details such as title, artist, album, and album artwork.",
+      "PWA (Progressive Web App) support. ",
+    ],
+    tags: [
+      {
+        name: "HTML",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "CSS",
+        color: "green-text-gradient",
+      },
+      {
+        name: "JavaScript",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "Springboot",
+        color: "orange-text-gradient",
+      },
+      {
+        name: "MySQL",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "thymeLeaf",
+        color: "green-text-gradient",
+      },
+    ],
+    image: def,
+    source_code_link: "https://github.com/abhiishekpanchal/Library-Management-System-",
+    live_link: "https://hayasaka.live/",
+  },
+];
+
+// const experiences = [
+//   {
+//     title: "React.js Developer",
+//     company_name: "Starbucks",
+//     icon: starbucks,
+//     iconBg: "#383E56",
+//     date: "March 2020 - April 2021",
+//     points: [
+//       "Developing and maintaining web applications using React.js and other related technologies.",
+//       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+//       "Implementing responsive design and ensuring cross-browser compatibility.",
+//       "Participating in code reviews and providing constructive feedback to other developers.",
+//     ],
+//   },
+//   {
+//     title: "React Native Developer",
+//     company_name: "Tesla",
+//     icon: tesla,
+//     iconBg: "#E6DEDD",
+//     date: "Jan 2021 - Feb 2022",
+//     points: [
+//       "Developing and maintaining web applications using React.js and other related technologies.",
+//       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+//       "Implementing responsive design and ensuring cross-browser compatibility.",
+//       "Participating in code reviews and providing constructive feedback to other developers.",
+//     ],
+//   },
+//   {
+//     title: "Web Developer",
+//     company_name: "Shopify",
+//     icon: shopify,
+//     iconBg: "#383E56",
+//     date: "Jan 2022 - Jan 2023",
+//     points: [
+//       "Developing and maintaining web applications using React.js and other related technologies.",
+//       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+//       "Implementing responsive design and ensuring cross-browser compatibility.",
+//       "Participating in code reviews and providing constructive feedback to other developers.",
+//     ],
+//   },
+//   {
+//     title: "Full stack Developer",
+//     company_name: "Meta",
+//     icon: meta,
+//     iconBg: "#E6DEDD",
+//     date: "Jan 2023 - Present",
+//     points: [
+//       "Developing and maintaining web applications using React.js and other related technologies.",
+//       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+//       "Implementing responsive design and ensuring cross-browser compatibility.",
+//       "Participating in code reviews and providing constructive feedback to other developers.",
+//     ],
+//   },
+// ];
+
+// const testimonials = [
+//   {
+//     testimonial:
+//       "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+//     name: "Sara Lee",
+//     designation: "CFO",
+//     company: "Acme Co",
+//     image: "https://randomuser.me/api/portraits/women/4.jpg",
+//   },
+//   {
+//     testimonial:
+//       "I've never met a web developer who truly cares about their clients' success like Rick does.",
+//     name: "Chris Brown",
+//     designation: "COO",
+//     company: "DEF Corp",
+//     image: "https://randomuser.me/api/portraits/men/5.jpg",
+//   },
+//   {
+//     testimonial:
+//       "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+//     name: "Lisa Wang",
+//     designation: "CTO",
+//     company: "456 Enterprises",
+//     image: "https://randomuser.me/api/portraits/women/6.jpg",
+//   },
+// ];
+
+export {
+  services,
+  technologies,
+  //  experiences,
+  //  testimonials,
+  projects,
+};
